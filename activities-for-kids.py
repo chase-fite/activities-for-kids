a7ab239224c9b13ecd9fb2ae3def26d5ef54839c
Chase Fite <chasef7031@gmail.com>
2020-01-14 05:59:14 -0600

    finished exercise

---
+++ activities-for-kids.py
@@ -0,0 +1,28 @@
+def run(name):
+    return print(f'{name} ran around the yard')
+
+def swing(name):
+    return print(f'{name} swang on the swing')
+
+def slide(name):
+    return print(f'{name} slid down the slide')
+
+def hide_and_seek(name):
+    return print(f'{name} played hide and seek')
+
+running_kids = ["Pam", "Sam", "Andrea", "Will"]
+swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+
+for kid in running_kids:
+    run(kid)
+
+for kid in swinging_kids:
+    swing(kid)
+
+for kid in sliding_kids:
+    slide(kid)
+
+for kid in hiding_kids:
+    hide_and_seek(kid)
